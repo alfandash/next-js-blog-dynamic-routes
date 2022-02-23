89c3efd2ae04ffb526a1176f0c5f750b10a05be2
alfandash <cumiasem91@gmail.com>
2022-02-23 13:08:47 +0700

    part12: add favicon in header based on wordpress

--- frontend/src/components/layout/index.js
+++ frontend/src/components/layout/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Footer from "./footer";
 import Header from "./header";
 
@@ -5,6 +6,9 @@ const Layout = ({ children, data }) => {
   return (
     <>
       <div>
+        <Head>
+          <link rel="shortcut icon" href={ data?.header?.favicon} />
+        </Head>
         <Header header={data?.header} headerMenus={data?.menus?.headerMenus} />
         <div>{children}</div>
         <Footer footer={data?.footer} footerMenus={data?.menus?.footerMenus} />
